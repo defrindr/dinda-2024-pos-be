11f6c9fe1b1377ad38174819f5942a561b5ff008
Defri Indra Mahardika <defrindr@gmail.com>
2024-06-29 12:26:49 +0700

    fix: remove LaravelAES

--- database/migrations/2024_02_05_183851_functions.php
+++ database/migrations/2024_02_05_183851_functions.php
@@ -10,39 +10,6 @@ return new class extends Migration
      */
     public function up(): void
     {
-        DB::statement('DROP FUNCTION IF EXISTS LaravelAES');
-        DB::statement("
-            CREATE FUNCTION LaravelAES(
-                encryption blob, 
-                salt blob, 
-                suffix VARCHAR(255)
-            ) RETURNS TEXT RETURN AES_DECRYPT(
-                FROM_BASE64(
-                JSON_UNQUOTE(
-                    JSON_EXTRACT(
-                    CONVERT(
-                        FROM_BASE64(encryption) USING utf8
-                    ), 
-                    '$.value'
-                    )
-                )
-                ), 
-                CONCAT(
-                FROM_BASE64(salt), 
-                suffix
-                ), 
-                FROM_BASE64(
-                JSON_UNQUOTE(
-                    JSON_EXTRACT(
-                    CONVERT(
-                        FROM_BASE64(encryption) USING utf8
-                    ), 
-                    '$.iv'
-                    )
-                )
-                )
-            );          
-        ");
     }
 
     /**
